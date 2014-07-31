f40a205e9314f7515c0e8950546b8da3fe8992a2
Jason Antman <jason@jasonantman.com>
2014-07-31 10:47:27 -0400

    class_spec finally passing

--- spec/acceptance/class_spec.rb
+++ spec/acceptance/class_spec.rb
@@ -6,12 +6,12 @@ describe 'rabbitmq class:' do
     package_name = 'rabbitmq-server'
     service_name = 'rabbitmq-server'
     package_source_281 = "http://www.rabbitmq.com/releases/rabbitmq-server/v2.8.1/rabbitmq-server-2.8.1-1.noarch.rpm"
-    package_ensure_281 = '2.8.1-1.noarch'
+    package_ensure_281 = '2.8.1-1'
   when 'SUSE'
     package_name       = 'rabbitmq-server'
     service_name       = 'rabbitmq-server'
     package_source_281 = "http://www.rabbitmq.com/releases/rabbitmq-server/v2.8.1/rabbitmq-server-2.8.1-1.noarch.rpm"
-    package_ensure_281 = '2.8.1-1.noarch'
+    package_ensure_281 = '2.8.1-1'
   when 'Debian'
     package_name       = 'rabbitmq-server'
     service_name       = 'rabbitmq-server'
@@ -119,6 +119,7 @@ describe 'rabbitmq class:' do
 
       # Apply twice to ensure no errors the second time.
       shell('yum -y erase rabbitmq-server')
+      shell('rm -Rf /var/lib/rabbitmq/mnesia /etc/rabbitmq')
       apply_manifest(pp, :catch_failures => true)
       expect(apply_manifest(pp, :catch_changes => true).exit_code).to be_zero
     end
