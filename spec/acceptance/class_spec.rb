f8b7644fff33c02b07eee2a4b22151e130b57aad
Jason Antman <jason@jasonantman.com>
2014-07-31 15:06:25 -0400

    move the 2.8.1 specs to their own file that runs last

--- spec/acceptance/class_spec.rb
+++ spec/acceptance/class_spec.rb
@@ -5,23 +5,15 @@ describe 'rabbitmq class:' do
   when 'RedHat'
     package_name = 'rabbitmq-server'
     service_name = 'rabbitmq-server'
-    package_source_281 = "http://www.rabbitmq.com/releases/rabbitmq-server/v2.8.1/rabbitmq-server-2.8.1-1.noarch.rpm"
-    package_ensure_281 = '2.8.1-1'
   when 'SUSE'
     package_name       = 'rabbitmq-server'
     service_name       = 'rabbitmq-server'
-    package_source_281 = "http://www.rabbitmq.com/releases/rabbitmq-server/v2.8.1/rabbitmq-server-2.8.1-1.noarch.rpm"
-    package_ensure_281 = '2.8.1-1'
   when 'Debian'
     package_name       = 'rabbitmq-server'
     service_name       = 'rabbitmq-server'
-    package_source_281 = ''
-    package_ensure_281 = '2.8.1'
   when 'Archlinux'
     package_name       = 'rabbitmq'
     service_name       = 'rabbitmq'
-    package_source_281 = ''
-    package_ensure_281 = '2.8.1'
   end
 
   context "default class inclusion" do
@@ -93,44 +85,6 @@ describe 'rabbitmq class:' do
 
       apply_manifest(pp_pre, :catch_failures => true)
       apply_manifest(pp, :catch_failures => true)
-    end
-
-    describe service(service_name) do
-      it { should be_enabled }
-      it { should be_running }
-    end
-  end
-
-  context "specified version 2.8.1-1" do
-    it 'should run successfully' do
-      pp = <<-EOS
-      class { 'rabbitmq':
-        version          => '2.8.1-1',
-        package_source   => '#{package_source_281}',
-        package_ensure   => '#{package_ensure_281}',
-        package_provider => 'rpm',
-        management_port  => '55672',
-      }
-      if $::osfamily == 'RedHat' {
-        class { 'erlang': epel_enable => true}
-        Class['erlang'] -> Class['rabbitmq']
-      }
-      EOS
-
-      # Apply twice to ensure no errors the second time.
-      shell('service rabbitmq-server stop')
-      shell('yum -y erase rabbitmq-server')
-      shell('rm -Rf /var/lib/rabbitmq/mnesia /etc/rabbitmq')
-      apply_manifest(pp, :catch_failures => true)
-      expect(apply_manifest(pp, :catch_changes => true).exit_code).to be_zero
-    end
-
-    describe command('rabbitmqctl status') do
-      its(:stdout) { should match /{rabbit,"RabbitMQ","2.8.1"}/ }
-    end
-
-    describe package(package_name) do
-      it { should be_installed }      
     end
 
     describe service(service_name) do
